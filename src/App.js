aee0e58ba427f0d848f95a1267e99df23ef93881
Luiz Renan <110012412+luiznwz@users.noreply.github.com>
2023-05-24 02:35:27 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
+
+  const [user, setUser] = useState({})
+
+
+  function handleRegister(e){
+    e.preventDefault();
+    
+    alert('Usuario Registrado com sucesso')
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    })
+  }
+  
+  return(
+    <div>
+      <h1>Cadastrando Usuario</h1>
+      <form onSubmit={handleRegister}>
+        <label>Nome:</label> <br/>
+        <input 
+        placeholder="Digite seu nome"
+        value={nome}
+        onChange={  (e) => setNome(e.target.value) } 
+        /><br/>
+
+        <label>Email:</label> <br/>
+        <input
+        placeholder="Digite seu email" 
+        value={email} 
+        onChange={  (e) => setEmail(e.target.value) }
+        /><br/>
+
+        <label>Idade:</label> <br/>
+        <input 
+        placeholder="Digite seu idade"
+        value={idade} 
+        onChange={  (e) => setIdade(e.target.value) } 
+        /><br/>
+
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <div>
+        <span>Bem Vindo: {user.nome} </span><br/>
+        <span>Idade: {user.idade} </span><br/>
+        <span>Email: {user.email} </span><br/>
+      </div>
+
     </div>
+
   );
 }
 
 export default App;
+
---
+++ src/components/Nome.js
@@ -0,0 +1,9 @@
+
+
+function Nome(props){
+    return(
+      <span>Bem-Vindo: {props.aluno} Idade: {props.idade} anos </span>
+    )
+  }
+
+  export default Nome;
